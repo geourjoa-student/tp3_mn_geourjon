f2aa52a5bc805fefd1b6591ecc33dbfa0ae2f204
geourjoa <anthony.geourjon@e.ujf-grenoble.fr>
2016-04-16 10:51:05 +0200

    Diag
    Écriture du programme diag

---
+++ diag.c
@@ -0,0 +1,31 @@
+/*
+ * diag.c
+ * 
+ * Copyright 2016 Anthony Geourjon <anthony.geourjon@e.ujf-grenoble.fr>
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "init.h"
+
+void diag(matricefloat_t A, vecteurfloat_t x, vecteurfloat_t b){
+    int i;
+    for(i=0;i<N;i++){
+        x[i]=b[i]/A[i][i];
+    }
+}
+
+int main(int argc, char **argv)
+{	
+	matricefloat_t m;
+    vecteurfloat_t v,res; 
+    
+    init_vect(v);
+    init_mat_diag(m);
+    diag(m,v,res);
+	
+	return 0;
+}
+
